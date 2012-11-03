1c1d86ae400cdedad6d60f879b843578be06ec93
DTailor <dann.croitoru@gmail.com>
2012-11-03 16:13:29 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,4 +1,4 @@
-sudo apt-get install zsh;
+sudo apt-get install git zsh;
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh;
 chsh -s `which zsh`;
 sudo shutdown -r 0
